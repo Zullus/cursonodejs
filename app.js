3f6c62d16737506968e89882cb960cc9d3c8630f
Edson Santos <edsonsantos@gmail.com>
2020-05-19 20:42:57 -0300

    Criando as rotas

--- app.js
+++ app.js
@@ -4,20 +4,25 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
 
-	res.send("<html><body>Portal de Notícias</body></html>")
-});
-
-app.get('/tecnologia', function(req, res){
-
-	res.render("sessao/tecnologia");
+    res.render("home/index");
 
 });
 
-app.listen(3000, function(){
+app.get('/formulario_inclusao_noticia', function(req, res) {
 
-	console.log('Servidor rodando com Express');
+    res.render("admin/form_add_noticia");
+
 });
 
+app.get('/noticias', function(req, res) {
 
+    res.render("noticias/noticias");
+
+});
+
+app.listen(3000, function() {
+
+    console.log('Servidor rodando com Express');
+});
